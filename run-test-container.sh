7007ba758771b474d74dcd5ac54901307e03ca87
Dawid Dziurla <dawidd0811@gmail.com>
2019-07-27 01:24:04 +0200

    add helper script

---
+++ run-test-container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# > ansible-playbook --ask-become-pass -i user@172.17.0.2, playbook.yml
+
+image="ansible-test"
+container="$image"
+
+clean() {
+    docker rm -f $container
+}
+
+trap clean SIGINT
+
+docker build -t $image .
+
+docker run \
+    -t \
+    --rm \
+    --name $container \
+    --security-opt \
+    seccomp=unconfined \
+    --tmpfs /run \
+    --tmpfs /run/lock \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    -v $HOME/.ssh:/home/user/.ssh:ro \
+    $image
