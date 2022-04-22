dd29199e3b16c62e7c0eae05a3df243759f9ad0e
Dawid Dziurla <dawidd0811@gmail.com>
2022-04-22 11:30:48 +0200

    scripts: move/remove

--- scripts/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-sudo apt-get update
-sudo apt-get install build-essential curl file git ansible python python-psutil
--- scripts/printer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-url="https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.1-1.gz"
-
-cd /tmp
-curl --silent "$url" | gunzip | sudo bash
