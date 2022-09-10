5567bbc01157f17224140581ac53fb886e8565bd
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-10 20:47:27 +0200

    fiddle around with bootstrap stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y ansible
+sudo apt-get install -y \
+    # for running the playbook, removed later in apt role
+    ansible \
+    # for Homebrew and Ansible, removed later in apt role and replaced in brew role
+    curl git \
+    # for Homebrew, not removed
+    build-essential zlib1g-dev
