bdf1a2bf5a4b251a1a86c1a44e37c404f623d868
Dawid Dziurla <dawidd0811@gmail.com>
2022-09-10 20:54:49 +0200

    bootstrap: fix syntax mhm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y \
-    ansible \ # for running the playbook, removed later in apt role
-    curl git \ # for Homebrew and Ansible, removed later in apt role and replaced in brew role
-    build-essential zlib1g-dev # for Homebrew, not removed
+sudo apt-get install -y build-essential zlib1g-dev curl git ansible
